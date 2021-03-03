d1e75720346088b41d3dfa770abc83db92045cd2
Neel Sheth <nsheth109@gmail.com>
2021-03-03 17:52:21 -0500

    added forloop to have data consistenlty updated on the HTML. Alose added a citycount for local storage

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 var apiKey = "b17fa481b192995d7aaa4c07ddf34c35";
 var sunButton = $(".sunButton");
 
+
+// Forloop for persisting the data onto HMTL page
+for (var i = 0; i < localStorage.length; i++) {
+
+    var city = localStorage.getItem(i);
+    // console.log(localStorage.getItem("City"));
+    var citySearch = $(".list-group").addClass("list-group-item");
+
+    citySearch.append("<li>" + city + "</li>");
+}
+// City count for local storage 
+var cityCount = 0;
